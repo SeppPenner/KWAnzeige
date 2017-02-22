ee0e2b396773da55aeb1bb9d11aa4a27c2419fb8
tim <jan-brenner@gmx.de>
2017-02-22 20:44:41 +0100

    Initial commit.

---
+++ KWAnzeige/Main.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Forms;
+using Languages.Implementation;
+using Languages.Interfaces;
+using Timer = System.Timers.Timer;
+
+namespace KWAnzeige
+{
+    public partial class Main : Form
+    {
+        private readonly ILanguageManager _lm = new LanguageManager();
+        private readonly Timer _timerCw = new Timer();
+
+        public Main()
+        {
+            InitializeComponent();
+            InitializeLanguageManager();
+            LoadLanguagesToCombo();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            textBoxCW.Text = GetIso8601WeekOfYear(DateTime.Now).ToString();
+            _timerCw.Elapsed += TimerCW_Tick;
+            _timerCw.Interval = 1000;
+            _timerCw.Start();
+        }
+
+        private void ButtonMinimize_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void TimerCW_Tick(object sender, EventArgs e)
+        {
+            textBoxCW.Text = GetIso8601WeekOfYear(DateTime.Now).ToString();
+        }
+
+        private int GetIso8601WeekOfYear(DateTime time)
+        {
+            var day = CultureInfo.InvariantCulture.Calendar.GetDayOfWeek(time);
+            if (day >= DayOfWeek.Monday && day <= DayOfWeek.Wednesday)
+                time = time.AddDays(3);
+            return CultureInfo.InvariantCulture.Calendar.GetWeekOfYear(time, CalendarWeekRule.FirstFourDayWeek,
+                DayOfWeek.Monday);
+        }
+
+        private void InitializeLanguageManager()
+        {
+            _lm.SetCurrentLanguage("de-DE");
+            _lm.OnLanguageChanged += OnLanguageChanged;
+        }
+
+        private void LoadLanguagesToCombo()
+        {
+            foreach (var lang in _lm.GetLanguages())
+                comboBoxLanguage.Items.Add(lang.Name);
+            comboBoxLanguage.SelectedIndex = 0;
+        }
+
+        private void comboBoxLanguage_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBoxLanguage.SelectedItem.ToString())
+            {
+                case "Deutsch":
+                    _lm.SetCurrentLanguage("de-DE");
+                    break;
+                case "English (US)":
+                    _lm.SetCurrentLanguage("en-US");
+                    break;
+            }
+        }
+
+        private void OnLanguageChanged(object sender, EventArgs eventArgs)
+        {
+            labelCW.Text = _lm.GetCurrentLanguage().GetWord("TodaysCW");
+            Text = _lm.GetCurrentLanguage().GetWord("Title");
+            buttonMinimize.Text = _lm.GetCurrentLanguage().GetWord("Minimize");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+KWAnzeige
+====================================
+
+KWAnzeige is an executable to check the current calendar week.
+The executable was written and tested in .Net 4.6.2.
+
+[![Build status](https://ci.appveyor.com/api/projects/status/3k72g5f5m4hicirq?svg=true)](https://ci.appveyor.com/project/SeppPenner/thedummyproject)
+
+Change history
+--------------
+
+* **Version 1.0.0.1 (2017-02-22)** : Switched to .Net to 4.6.2, Refactored code.
+* **Version 1.0.0.0 (2017-02-22)** : 1.0 release.
