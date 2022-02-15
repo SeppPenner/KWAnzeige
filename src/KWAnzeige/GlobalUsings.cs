aec9ce8ee052597b50cf26f62eb669f262f5b726
SeppPenner <HansM2013@gmx.de>
2022-02-15 16:04:16 +0100

    Added nullable checks, added editorconfig, added file scoped namespaces, added global usings.

---
+++ src/KWAnzeige/GlobalUsings.cs
@@ -0,0 +1,8 @@
+#pragma warning disable IDE0065 // Die using-Anweisung wurde falsch platziert.
+global using System.Globalization;
+
+global using Languages.Implementation;
+global using Languages.Interfaces;
+
+global using Timer = System.Timers.Timer;
+#pragma warning restore IDE0065 // Die using-Anweisung wurde falsch platziert.
